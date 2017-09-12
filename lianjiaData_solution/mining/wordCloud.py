100acf325ccc46787b529f453b6c5978fec24de4
kaibao <kaibao@cisco.com>
2017-09-12 23:25:44 +0800

    init project

---
+++ lianjiaData_solution/mining/wordCloud.py
@@ -0,0 +1,27 @@
+#-*- coding=utf8-*-
+
+import jieba
+#import 
+
+
+Image():
+    data = []
+    jieba.analyse.set_stop_words("./stopwords.txt")
+
+    with codecs.open("weibo1.txt", 'r', encoding="utf-8") as f:
+        for text in f.readlines():
+            data.extend(jieba.analyse.extract_tags(text, topK=20))
+        data = " ".join(data)
+        mask_img = imread('./52f90c9a5131c.jpg', flatten=True)
+        wordcloud = WordCloud(
+            font_path='msyh.ttc',
+            background_color='white',
+            mask=mask_img
+        ).generate(data)
+        plt.imshow(wordcloud.recolor(color_func=grey_color_func, random_state=3),
+                   interpolation="bilinear")
+        plt.axis('off')
+        plt.savefig('./heart2.jpg', dpi=1600)
+
+
+
---
+++ lianjiaData_solution/spiders/spider.py
@@ -0,0 +1,10 @@
+#-*- coding=utf8 -*-
+
+
+import os
+from scrapy import log
+form scrapy.spiders import CrawlSpider
+from scrapy.selector import Selector
+
+class Data(CrawlSpider):
+
